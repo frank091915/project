44e6324304e84fca62ae9be83d427615ca2f53c3
frank091915 <frank091915@163.com>
2019-01-08 21:28:39 +0800

    列表页渲染

--- src/js/component/productInfo.js
+++ src/js/component/productInfo.js
@@ -1,5 +1,21 @@
 require(["../require-config"],function(){
-	require(["jquery","header","footer","fdj"],function(){
-		alert(123);
+	require(["jquery","url","header","footer","fdj"],function($,url){
+		
+		$.ajax({
+			url:url.apiUrl+"/singleProduct",
+			type:"get",
+			success:function(res){
+				if(res.res_code==1){
+					console.log(res);
+					$(".singleProduct").attr({src:res.res_body.img});
+					$(".productTitle").html(res.res_body.title);
+					$(".price").html(res.res_body.price);
+					$(".product-description").html(res.res_body.description);
+				}
+
+			}
+		});
+		
+		
 	})
 })
